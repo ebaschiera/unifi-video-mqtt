75143b0487df52d80d0fd9fdeffe2ee35655a31d
Ermanno Baschiera <ebaschiera@gmail.com>
2019-08-24 16:05:27 +0200

    fix camera id parsing on newer unifi video (tested on 3.10.2)

--- unifi-video-mqtt.sh
+++ unifi-video-mqtt.sh
@@ -42,7 +42,7 @@ while inotifywait -e modify $UNIFI_MOTION_LOG; do
 
   if [[ $VER_TEST == "[uv.analytics.motion]" ]]; then
     # New Format
-    LAST_CAM=`echo $LAST_MESSAGE | awk -F '[][]' '{print $6}' | cut -d '|' -f 1`
+    LAST_CAM=`echo $LAST_MESSAGE | awk -F '[][]' '{print $4}'
   else
     # Old Format
     LAST_CAM=`echo $LAST_MESSAGE | awk -F '[][]' '{print $2}'`
